b2a8b95e587687d8f7b82c3fad332ad8c3bdc690
V Vetsin <vilinamogili423@gmail.com>
2024-08-19 20:51:52 +0200

    removed cookie shit from global layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -28,87 +28,8 @@
     });
   }
 
-  async function sendSubscriptonToServer(subscription: PushSubscription) {
-    try {
-      const res = await fetch("/api/addSubscription", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ uid: $user?.uid, subscription }),
-      });
-      if (!res.ok)
-        throw new Error(
-          `Error saving subscription on server: ${res.statusText} (${res.status})`
-        );
-    } catch (error) {
-      console.error("Error sending subscription to server:", error);
-      unsubscribe();
-    }
-  }
-
-  async function checkSubscriptionStatus() {
-    if ("serviceWorker" in navigator) {
-      const registration = await navigator.serviceWorker.ready;
-      const subscription = await registration.pushManager.getSubscription();
-      console.log("Push subscription checked:", JSON.stringify(subscription));
-      const exists = subscription !== null;
-      if (exists) {
-        sendSubscriptonToServer(subscription);
-      }
-    }
-    return false;
-  }
-
-  async function subscribeUser() {
-    if ("serviceWorker" in navigator) {
-      try {
-        console.log("trying to subscribe");
-        const res = await fetch("/api/vapidPubKey");
-        const { data } = await res.json();
-        console.log(data);
-
-        const registration = await navigator.serviceWorker.ready;
-        const subscription = await registration.pushManager.subscribe({
-          userVisibleOnly: true,
-          applicationServerKey: data,
-        });
-
-        console.log("Push subscription:", JSON.stringify(subscription));
-        sendSubscriptonToServer(subscription);
-      } catch (error) {
-        console.error("Error subscribing user to push notifications:", error);
-      }
-    }
-  }
-
-  async function unsubscribe() {
-    if ("serviceWorker" in navigator) {
-      const registration = await navigator.serviceWorker.ready;
-      const subscription = await registration.pushManager.getSubscription();
-      if (subscription) {
-        await subscription.unsubscribe();
-        console.log(
-          "Push subscription unsubscribed:",
-          JSON.stringify(subscription)
-        );
-      }
-    }
-  }
-
   onMount(async () => {
     detectSWUpdate();
-    if ($user) {
-      if (Notification.permission !== "granted") {
-        Notification.requestPermission();
-        if ((await checkSubscriptionStatus()) == false) {
-          subscribeUser();
-        } else {
-        }
-      } else {
-        console.log("user is subscribed");
-      }
-    }
   });
 </script>
 
